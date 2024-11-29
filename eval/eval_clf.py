d08445c6e1352eddbe6984ec68a2e4a7beb947b5
Markus Müller <markus.j.mueller94@icloud.com>
2024-11-29 19:25:46 +0100

    first full run

--- eval/eval_clf.py
+++ eval/eval_clf.py
@@ -3,6 +3,22 @@ import logging
 from datetime import datetime
 import pandas as pd
 import argparse
+import json 
+import pickle
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, accuracy_score, f1_score
+from sklearn.svm import SVC
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+from xgboost import XGBClassifier
+import lightgbm as lgb
+
 
 parser = argparse.ArgumentParser(
     description="Evaulation Classifier"
@@ -52,6 +68,18 @@ else:
 
 print(RUN_ID)
 
+# load dataset info JSON
+info_path = f"data/raw/{DATASET}/{DATASET}.json"
+if not os.path.exists(info_path):
+    raise FileNotFoundError(f"The file does not exists")
+else:
+    print("info.json found")
+
+# load info json
+with open(info_path, "r") as f:
+    info = json.load(f)
+
+
 ### 
 # logger definition
 ###
@@ -82,6 +110,40 @@ logger.info(f"Dataset: {DATASET}")
 logger.info(f"Synthetic Data Generation Algo: {SDG}")
 logger.info(f"Type of Run: {RUN}")
 
+def impute_missing_values(df):
+    # Category: repalce na with "missing"
+    cat_columns_idx = info["cat_col_idx"]
+    columns_names = info["column_names"]
+    cat_columns_names = [columns_names[i] for i in cat_columns_idx]
+
+    for col in cat_columns_names:
+        df[col] = df[col].fillna('Missing')
+    
+    return df
+
+
+def apply_onehot_encoding(train_df, test_df, categorical_columns):
+    # Create OneHotEncoder
+    encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
+
+    # Apply ColumnTransformer to the categorical columns
+    preprocessor = ColumnTransformer(
+        transformers=[(col, encoder, [col]) for col in categorical_columns],
+        remainder='passthrough'  # Keep non-categorical columns unchanged
+    )
+    
+    # Fit the encoder on the train data and transform both train and test data
+    train_encoded = preprocessor.fit_transform(train_df)
+    test_encoded = preprocessor.transform(test_df)
+
+    # Convert the encoded data back to DataFrame
+    train_encoded_df = pd.DataFrame(train_encoded, columns=preprocessor.get_feature_names_out())
+    test_encoded_df = pd.DataFrame(test_encoded, columns=preprocessor.get_feature_names_out())
+    
+    # Return the encoded DataFrames
+    return train_encoded_df, test_encoded_df
+
+
 
 def sdg_run():
     print("sdg-run")
@@ -99,9 +161,161 @@ def sdg_run():
     # combine syn and train
     df_train = pd.concat([df_train_ci, df_train_syn])
 
-    print("Shape df_train:", df_train.shape)
-    print("Shape df_test:", df_test.shape)
+    target_col_idx = info["target_col_idx"][0]
+    target_col_name = info["column_names"][target_col_idx]
+    print("Traget column:", target_col_idx)
+    print("Target column name:", target_col_name)
+    
+    counts = df_train[target_col_name].value_counts()
+    
+    # log dataset infos
+    logger.info("-"*50)
+    logger.info("Dataset infos")
+    logger.info(counts)
+    logger.info(f"Shape df_train: {df_train.shape}")
+    logger.info(f"Shape df_test: {df_test.shape}")
 
+
+    # pre process
+    # certain classification models need numbers as the y variable, like XGBoost
+    # 1 for minority class and 0 for majority class
+    df_train[target_col_name] = df_train[target_col_name].map({info["maj_class"]:0, info["min_class"]: 1}) 
+    df_test[target_col_name] = df_test[target_col_name].map({info["maj_class"]:0, info["min_class"]: 1}) 
+
+    cat_columns_idx = info["cat_col_idx"]
+    columns_names = info["column_names"]
+    cat_columns_names = [columns_names[i] for i in cat_columns_idx]
+
+    # impute misisng values
+    df_train = impute_missing_values(df_train)
+    df_test = impute_missing_values(df_test)
+
+    # on hot encode categorical data 
+    df_train, df_test = apply_onehot_encoding(df_train, df_test, cat_columns_names)
+
+    print("NaNs in df_train:", df_train.isnull().values.any())
+    print("NaNs in df_test:", df_test.isnull().values.any())
+
+    # split data
+    X_train = df_train.drop(columns=[f"remainder__{target_col_name}"])
+    y_train = df_train[f"remainder__{target_col_name}"]
+
+    X_test = df_test.drop(columns=[f"remainder__{target_col_name}"])
+    y_test = df_test[f"remainder__{target_col_name}"]
+
+    logger.info("-"*50)
+    logger.info("Shape of train and test data")
+    logger.info(f"Timestamp: {datetime.now().strftime('%Y%m%d_%H%M%S')}")
+    logger.info(f"X_train: {X_train.shape}")
+    logger.info(f"y_train: {y_train.shape}")
+    logger.info(f"X_test: {X_test.shape}")
+    logger.info(f"y_test: {y_test.shape}")
+
+    logger.info("-"*50)
+    logger.info(f"Stating training of classifier: {datetime.now().strftime('%Y%m%d_%H%M%S')}")
+
+    clfs = { 
+        "Logistic Regression": LogisticRegression(random_state=42),
+        "Random Forest": RandomForestClassifier(random_state=42),
+        "XGBoost":  XGBClassifier(random_state=42),
+        "AdaBoost": AdaBoostClassifier(random_state=42, algorithm="SAMME"),
+        "LightGBM": lgb.LGBMClassifier(random_state=42),
+        "KNeighbors": KNeighborsClassifier(),
+        "SVM": SVC(random_state=42)
+    }
+
+    logger.info("Classification Models:")
+    for name, clf in clfs.items():
+        logger.info(f"{name}: {clf.get_params()}")
+    
+
+    # save f1_socres in results dict
+    results = {}
+
+    for name, clf in clfs.items():
+        logger.info(f"Results for {name}")
+        # train model
+        clf.fit(X_train, y_train)
+
+        # predcit
+        y_pred = clf.predict(X_test)
+
+        # calcualte f1_score
+        accuracy_score_value = accuracy_score(y_test, y_pred)
+        f1_score_value = float(f1_score(list(y_test), list(y_pred), pos_label=1))
+        # log resutls
+
+        logger.info(f"Accuracy Score: {accuracy_score_value}")
+        logger.info(f"F1-Score of minority class: {f1_score_value}")
+        logger.info(f"{classification_report(y_test, y_pred)}")
+
+        # add results to dict
+        results[name] = (accuracy_score_value, f1_score_value)
+
+
+    logger.info(f"Finished training of classifier: {datetime.now().strftime('%Y%m%d_%H%M%S')}")
+    logger.info("-"*50)
+    logger.info("Saving results...") 
+
+    if not os.path.exists("results/results_df_log.pkl"):
+        # init pandas df and save
+        df_save = pd.DataFrame([
+            {
+                "ID": 0,
+                "Timestamp": TS,
+                "RUN": "init",
+                "SDG": "none",
+                "LR_accuracy": results["Logistic Regression"][0],
+                "LR_f1-score": results["Logistic Regression"][1],
+                "RF_accuracy": results["Random Forest"][0],
+                "RF_f1-score": results["Random Forest"][1],
+                "XGB_accuracy": results["XGBoost"][0],
+                "XGB_f1-score": results["XGBoost"][1],
+                "Ada_accuracy": results["AdaBoost"][0],
+                "Ada_f1-score": results["AdaBoost"][1],
+                "LGBM_accuracy": results["LightGBM"][0],
+                "LGBM_f1-score": results["LightGBM"][1],
+                "KNN_accuracy": results["KNeighbors"][0],
+                "KNN_f1-score": results["KNeighbors"][1],
+                "SVM_accuracy": results["SVM"][0],
+                "SVM_f1-score": results["SVM"][1]
+            }
+        ])
+
+        df_save.to_pickle("results/results_df_log.pkl")
+        df_save.to_csv("results/results_df_log.csv")
+    else:
+        # read pandas and append results
+        df = pd.read_pickle("results/result_df_log.pkl")
+        
+        df_new = pd.DataFrame([
+            {
+                "ID": RUN_ID,
+                "Timestamp": TS,
+                "RUN": "init",
+                "SDG": "none",
+                "LR_accuracy": results["Logistic Regression"][0],
+                "LR_f1-score": results["Logistic Regression"][1],
+                "RF_accuracy": results["Random Forest"][0],
+                "RF_f1-score": results["Random Forest"][1],
+                "XGB_accuracy": results["XGBoost"][0],
+                "XGB_f1-score": results["XGBoost"][1],
+                "Ada_accuracy": results["AdaBoost"][0],
+                "Ada_f1-score": results["AdaBoost"][1],
+                "LGBM_accuracy": results["LightGBM"][0],
+                "LGBM_f1-score": results["LightGBM"][1],
+                "KNN_accuracy": results["KNeighbors"][0],
+                "KNN_f1-score": results["KNeighbors"][1],
+                "SVM_accuracy": results["SVM"][0],
+                "SVM_f1-score": results["SVM"][1]
+            }
+        ])
+
+        df_save = pd.concat([df, df_new])
+        df_save.to_pickle("results/results_df_log.pkl")
+        df_save.to_csv("results/results_df_log.csv")
+
+    logger.info("Results saved")
 
 
 def baseline_run():
