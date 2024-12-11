d64c32d652d7c71bfe4d52cf9ee12a5adcaa848f
Markus Müller <markus.j.mueller94@icloud.com>
2024-12-11 18:26:07 +0100

    quick changes

--- sdg-models/ctab-gan-plus/main.py
+++ sdg-models/ctab-gan-plus/main.py
@@ -29,23 +29,20 @@ synthesizer = CTABGAN(
     mixed_columns={"capital.loss": [0.0], "capital.gain": [0.0]},
     general_columns=["age"],
     non_categorical_columns=[],
-    integer_columns=["age", "fnlwgt", "capital.gain", "capital.loss", "hours.per.week", "education.num"],
+    integer_columns=["age", "fnlwgt", "capital.gain", "capital.loss", "hours.per.week", "educatioe.num"],
     problem_type={"Classification": "income"},
 )
 
 
-synthesizer.fit()
-
-
-#for i in range(num_exp):
-#    synthesizer.fit()
-#    syn = synthesizer.generate_samples()
-#    syn.to_csv(
-#        fake_file_root
-#        + "/"
-#        + dataset
-#        + "/"
-#        + dataset
-#        + "_fake_{exp}.csv".format(exp=i),
-#        index=False,
-#    )
+for i in range(num_exp):
+    synthesizer.fit()
+    syn = synthesizer.generate_samples()
+    syn.to_csv(
+        fake_file_root
+        + "/"
+        + dataset
+        + "/"
+        + dataset
+        + "_fake_{exp}.csv".format(exp=i),
+        index=False,
+    )
