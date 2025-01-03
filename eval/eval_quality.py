e322752ab48713cb8a65717f064ccd5f4e435d5b
Markus Müller <markus.j.mueller94@icloud.com>
2025-01-03 09:33:09 +0100

    added sdv quality report to eval step

--- eval/eval_quality.py
+++ eval/eval_quality.py
@@ -11,9 +11,12 @@ from scipy.stats import wasserstein_distance
 from sklearn.preprocessing import MinMaxScaler
 from datetime import datetime
 
-from synthcity.metrics import eval_detection, eval_performance, eval_statistical
-from synthcity.plugins.core.dataloader import GenericDataLoader
-from sklearn.preprocessing import OneHotEncoder
+from sdv.evaluation.single_table import run_diagnostic, evaluate_quality
+from sdv.metadata import Metadata
+
+# from synthcity.metrics import eval_detection, eval_performance, eval_statistical
+# from synthcity.plugins.core.dataloader import GenericDataLoader
+# from sklearn.preprocessing import OneHotEncoder
 
 parser = argparse.ArgumentParser(description="Evaluation plots")
 
@@ -68,104 +71,135 @@ for col in cat_cols:
     df_syn[col] = df_syn[col].replace(0.0, "missing")
 
 
+# generte metadata for sdv evalution metrics
+metadata = Metadata.detect_from_dataframe(data=df_real, table_name=DATASET)
+
+# create diagnostic and quality report from sdv
+diagnostic_report = run_diagnostic(
+    real_data=df_real, synthetic_data=df_syn, metadata=metadata, verbose=True
+)
+quality_report = evaluate_quality(
+    real_data=df_real, synthetic_data=df_syn, metadata=metadata, verbose=True
+)
+
+# Save scores
+data_validity_score = diagnostic_report.get_details(property_name="Data Validity")[
+    "Score"
+].mean()
+data_structure_score = diagnostic_report.get_details(property_name="Data Structure")[
+    "Score"
+].mean()
+column_shape_score = quality_report.get_details(property_name="Column Shapes")[
+    "Score"
+].mean()
+column_pair_trends_score = quality_report.get_details(
+    property_name="Column Pair Trends"
+)["Score"].mean()
+
+
 ### Statistical similarity
 # code from ctab-gan-plus repo
-def eval_stat():
-    really = df_real.copy()
-    fakey = df_syn.copy()
+really = df_real.copy()
+fakey = df_syn.copy()
 
-    # create corrleation matrix
-    real_corr = compute_associations(df_real, nominal_columns=cat_cols)
-    syn_corr = compute_associations(df_syn, nominal_columns=cat_cols)
+# create corrleation matrix
+real_corr = compute_associations(df_real, nominal_columns=cat_cols)
+syn_corr = compute_associations(df_syn, nominal_columns=cat_cols)
 
-    corr_dist = np.linalg.norm(real_corr - syn_corr)
+corr_dist = np.linalg.norm(real_corr - syn_corr)
 
-    Stat_dict = {}
-    cat_stat = []
-    num_stat = []
+Stat_dict = {}
+cat_stat = []
+num_stat = []
 
-    for column in df_real.columns:
+for column in df_real.columns:
 
-        if column in cat_cols:
-            # print(column)
+    if column in cat_cols:
+        # print(column)
 
-            real_pdf = (
-                really[column].value_counts() / really[column].value_counts().sum()
+        real_pdf = really[column].value_counts() / really[column].value_counts().sum()
+        fake_pdf = fakey[column].value_counts() / fakey[column].value_counts().sum()
+        categories = (
+            (fakey[column].value_counts() / fakey[column].value_counts().sum())
+            .keys()
+            .tolist()
+        )
+        sorted_categories = sorted(categories)
+
+        real_pdf_values = []
+        fake_pdf_values = []
+
+        for i in sorted_categories:
+            real_pdf_values.append(real_pdf[i])
+            fake_pdf_values.append(fake_pdf[i])
+
+        if len(real_pdf) != len(fake_pdf):
+            zero_cats = set(really[column].value_counts().keys()) - set(
+                fakey[column].value_counts().keys()
             )
-            fake_pdf = fakey[column].value_counts() / fakey[column].value_counts().sum()
-            categories = (
-                (fakey[column].value_counts() / fakey[column].value_counts().sum())
-                .keys()
-                .tolist()
-            )
-            sorted_categories = sorted(categories)
+            for z in zero_cats:
+                real_pdf_values.append(real_pdf[z])
+                fake_pdf_values.append(0)
+        Stat_dict[column] = distance.jensenshannon(
+            real_pdf_values, fake_pdf_values, 2.0
+        )
+        cat_stat.append(Stat_dict[column])
+        # print("column: ", column, "JSD: ", Stat_dict[column])
+    else:
+        scaler = MinMaxScaler()
+        scaler.fit(df_real[column].values.reshape(-1, 1))
+        l1 = scaler.transform(df_real[column].values.reshape(-1, 1)).flatten()
+        l2 = scaler.transform(df_syn[column].values.reshape(-1, 1)).flatten()
+        Stat_dict[column] = wasserstein_distance(l1, l2)
+        # print("column: ", column, "WD: ", Stat_dict[column])
+        num_stat.append(Stat_dict[column])
 
-            real_pdf_values = []
-            fake_pdf_values = []
+print(np.mean(num_stat), np.mean(cat_stat), corr_dist)
 
-            for i in sorted_categories:
-                real_pdf_values.append(real_pdf[i])
-                fake_pdf_values.append(fake_pdf[i])
-
-            if len(real_pdf) != len(fake_pdf):
-                zero_cats = set(really[column].value_counts().keys()) - set(
-                    fakey[column].value_counts().keys()
-                )
-                for z in zero_cats:
-                    real_pdf_values.append(real_pdf[z])
-                    fake_pdf_values.append(0)
-            Stat_dict[column] = distance.jensenshannon(
-                real_pdf_values, fake_pdf_values, 2.0
-            )
-            cat_stat.append(Stat_dict[column])
-            # print("column: ", column, "JSD: ", Stat_dict[column])
-        else:
-            scaler = MinMaxScaler()
-            scaler.fit(df_real[column].values.reshape(-1, 1))
-            l1 = scaler.transform(df_real[column].values.reshape(-1, 1)).flatten()
-            l2 = scaler.transform(df_syn[column].values.reshape(-1, 1)).flatten()
-            Stat_dict[column] = wasserstein_distance(l1, l2)
-            # print("column: ", column, "WD: ", Stat_dict[column])
-            num_stat.append(Stat_dict[column])
-
-    print(np.mean(num_stat), np.mean(cat_stat), corr_dist)
-
-    # check if there is already a csv to save the results
-    if not os.path.exists("results/quality_data.csv"):
-        ts = datetime.now().strftime("%Y%m%d_%H%M%S")
-        init_data = [
-            {
-                "Timestamp": ts,
-                "Syn_Algo": "init",
-                "Dataset": "init",
-                "Average WD (Continuous Columns)": 0,
-                "Average JSD (Categorical Columns)": 0,
-                "Correlation Distance": 0,
-            }
-        ]
-        resutls_df = pd.DataFrame(init_data)
-        resutls_df.to_csv("results/quality_data.csv", index=False)
-
-    df_old = pd.read_csv("results/quality_data.csv")
-
+# check if there is already a csv to save the results
+if not os.path.exists("results/quality_data.csv"):
     ts = datetime.now().strftime("%Y%m%d_%H%M%S")
-    new_data = [
+    init_data = [
         {
             "Timestamp": ts,
-            "Syn_Algo": SDG,
-            "Dataset": DATASET,
-            "Average WD (Continuous Columns)": np.mean(num_stat),
-            "Average JSD (Categorical Columns)": np.mean(cat_stat),
-            "Correlation Distance": corr_dist,
+            "Syn_Algo": "init",
+            "Dataset": "init",
+            "Validity Score": 0,
+            "Structure Score": 0,
+            "Column Shape Quality": 0,
+            "Column Pair Trends": 0,
+            "Average WD (Continuous Columns)": 0,
+            "Average JSD (Categorical Columns)": 0,
+            "Correlation Distance": 0,
         }
     ]
+    resutls_df = pd.DataFrame(init_data)
+    resutls_df.to_csv("results/quality_data.csv", index=False)
 
-    df_new = pd.DataFrame(new_data)
+df_old = pd.read_csv("results/quality_data.csv")
 
-    df_new = pd.concat([df_old, df_new])
-    df_new.to_csv("results/quality_data.csv", index=False)
+ts = datetime.now().strftime("%Y%m%d_%H%M%S")
+new_data = [
+    {
+        "Timestamp": ts,
+        "Syn_Algo": SDG,
+        "Dataset": DATASET,
+        "Validity Score": data_validity_score,
+        "Structure Score": data_structure_score,
+        "Column Shape Quality": column_shape_score,
+        "Column Pair Trends": column_pair_trends_score,
+        "Average WD (Continuous Columns)": np.mean(num_stat),
+        "Average JSD (Categorical Columns)": np.mean(cat_stat),
+        "Correlation Distance": corr_dist,
+    }
+]
 
-    print(df_new)
+df_new = pd.DataFrame(new_data)
+
+df_new = pd.concat([df_old, df_new])
+df_new.to_csv("results/quality_data.csv", index=False)
+
+print(df_new)
 
 
 ### alpha-Precision & beta-Recall
@@ -246,7 +280,3 @@ def alpha_beta():
 
     Alpha_Precision_all = qual_res["delta_precision_alpha_naive"]
     Beta_Recall_all = qual_res["delta_coverage_beta_naive"]
-
-
-if __name__ == "__main__":
-    eval_stat()
